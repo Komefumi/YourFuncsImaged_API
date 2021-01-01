e51d0d962a81f02b76b8e9ba77fa11ea23230051
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-01-01 20:30:25 +0530

    Refine UsableFunction Model Create constructor

--- app/models/UsableFunction.go
+++ app/models/UsableFunction.go
@@ -1,9 +1,34 @@
-import "time"
+package models
 
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	util "github.com/Komefumi/YourFuncsImaged_API/app/util"
+)
+
+var(
+	// ErrFailedFuncCreate indicates failure to create Usable Function
+	ErrFailedFuncCreate = errors.New("Failed To Create New Func")
+)
+
+// UsableFunction struct to model UsableFunction
 type UsableFunction struct {
 	ID uint `gorm:"primaryKey;column:id;autoIncrement"`
 	Content string `gorm:"column:content"`
-	UserId uint
+	UserID uint `gorm:"column:user_id"`
 	User User
 	CreatedAt time.Time `gorm:"column:created_at"`
+}
+
+// CreateUsableFunc creates a new UsableFunction
+func CreateUsableFunc(userID uint, funcString string) error {
+	newFunc := UsableFunction{ UserID: userID, Content: funcString }
+	db := util.DBAccessorFunc()
+	if dbc:= db.Create(&newFunc); dbc.Error != nil {
+		fmt.Println(dbc.Error)
+		return ErrFailedFuncCreate
+	}
+	return nil
 }
